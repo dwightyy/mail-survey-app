8e0b72fefda93cf7f4273cdd2057ba8c353139ff
dwightyy <pribeiro.rib@gmail.com>
2019-06-09 14:33:37 -0300

    set dev and prod env

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const CookieSession = require("cookie-session");
+const passport = require("passport");
+
 require("./models/user");
 require("./services/passport");
 
@@ -8,6 +11,16 @@ const keys = require("./config/keys");
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
+
+app.use(
+    CookieSession({
+        maxAge: 15 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
